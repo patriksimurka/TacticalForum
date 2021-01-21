7683d796ab5e49782cef996889f7e29cfc988858
patriksimurka <patriksimurka@gmail.com>
2021-01-21 22:26:19 +0100

    working socket.io

--- main.py
+++ main.py
@@ -117,5 +117,10 @@ def chatroom():
 	return render_template('chatroom.html')
 
 
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+
 if __name__ == "__main__":
 	socketio.run(app)
