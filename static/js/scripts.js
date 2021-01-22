8d0c1c1e0fb4d3810813708c669b68776af26589
patriksimurka <patriksimurka@gmail.com>
2021-01-22 12:36:55 +0100

    active works

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -9,3 +9,16 @@ for (var i=0; i<m.length; i++) {
   }
 }, 5000);
 
+function set_active(){
+	console.log(window.location.href);
+	console.log();
+	for(i of $("nav li a")){
+		console.log(i.href);
+		if(i.href === window.location.href || i.href === window.location.href.substring(0, window.location.href.length - 1)){
+			$(i).parent().addClass('active');
+		}
+		else{
+			$(i).parent().removeClass('active');
+		}
+	}
+}
